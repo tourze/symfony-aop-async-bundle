567b5aa75366302972f22ef5dfe935e0dcc7ca47
github-ci <you@example.com>
2025-06-15 19:55:32 +0800

    新增 `tourze/bundle-dependency` 依赖并实现 `BundleDependencyInterface`：

--- src/AopAsyncBundle.php
+++ src/AopAsyncBundle.php
@@ -3,7 +3,17 @@
 namespace Tourze\Symfony\AopAsyncBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tourze\AsyncServiceCallBundle\AsyncServiceCallBundle;
+use Tourze\BundleDependency\BundleDependencyInterface;
+use Tourze\Symfony\Aop\AopBundle;
 
-class AopAsyncBundle extends Bundle
+class AopAsyncBundle extends Bundle implements BundleDependencyInterface
 {
+    public static function getBundleDependencies(): array
+    {
+        return [
+            AsyncServiceCallBundle::class => ['all' => true],
+            AopBundle::class => ['all' => true],
+        ];
+    }
 }
