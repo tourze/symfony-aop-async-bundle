401d08d0d6b416e1988f9d7c3e248f176bfdf413
github-ci <you@example.com>
2025-12-19 17:14:37 +0800

    upgrade

--- src/DependencyInjection/AopAsyncExtension.php
+++ src/DependencyInjection/AopAsyncExtension.php
@@ -4,7 +4,7 @@ namespace Tourze\Symfony\AopAsyncBundle\DependencyInjection;
 
 use Tourze\SymfonyDependencyServiceLoader\AutoExtension;
 
-class AopAsyncExtension extends AutoExtension
+final class AopAsyncExtension extends AutoExtension
 {
     protected function getConfigDir(): string
     {
